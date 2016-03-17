da1976300921a447e97cf1df8e3b94f960bc2287
37pin <ucorpor@gmail.com>
2016-03-17 10:33:12 +0100

    Introdution to HDFS

--- BigDataProject/src/java/dao/SongDescDao.java
+++ BigDataProject/src/java/dao/SongDescDao.java
@@ -2,26 +2,32 @@ package dao;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URI;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class SongDescDao {
 
-    public void loadData() {
+    public static final String IP = "10.154.101.249";
+    
+    public String loadData() {
+        String ans = "test: ";
         try {
-            Path pt = new Path("hdfs://10.154.101.249:9000/music/songsdesc.csv");
-            FileSystem fs = FileSystem.get(new Configuration());
+            Path pt = new Path("/music/songsdesc.csv");
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(new URI("webhdfs://" + IP + ":8020"), conf);
             BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
             String line;
             line = br.readLine();
             while (line != null) {
-                System.out.println(line);
+                ans += line;
                 line = br.readLine();
             }
         } catch (Exception e) {
-            System.out.println("Exception:\n" + e.getMessage());
+            e.printStackTrace();
         }
+        return ans;
     }
 
 }
